d561218add51f5ddc64ad9f55b85576289f9f3fc
Faiq <faiqradzali@gmail.com>
2020-01-07 13:31:48 +0800

    Stocklist Recyclerview + Searchview

---
+++ app/src/main/java/com/example/myapplication/RecyclerViewAdapter.java
@@ -0,0 +1,117 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> implements Filterable {
+
+    private static final String TAG = "RecyclerViewAdapter";
+
+    public RecyclerViewAdapter(Context context, ArrayList<String> stockName, ArrayList<String> stockNameFull) {
+        mContext = context;
+        mStockName = stockName;
+        mStockNameFull = stockNameFull;
+
+    }
+
+    private ArrayList<String> mStockName;
+    private ArrayList<String> mStockNameFull;
+    private Context mContext;
+
+    public RecyclerViewAdapter() {
+        mStockName = new ArrayList<>();
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listitem, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder: called");
+
+        holder.stockName.setText(mStockName.get(position));
+
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                Log.d(TAG, "onClick: clicked on: " + mStockName.get(position));
+
+                Toast.makeText(mContext, mStockName.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mStockName.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return exampleFilter;
+    }
+
+    private Filter exampleFilter = new Filter() {
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            ArrayList<String> filteredList = new ArrayList<>();
+
+            if (constraint == null || constraint.length() == 0) {
+                filteredList.addAll(mStockNameFull);
+            } else {
+                String filterPattern = constraint.toString().toLowerCase().trim();
+
+                for (String item : mStockNameFull) {
+                    if (item.toLowerCase().contains(filterPattern)) {
+                        filteredList.add(item);
+                    }
+                }
+            }
+
+            FilterResults results = new FilterResults();
+            results.values = filteredList;
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            mStockName.clear();
+            mStockName.addAll((ArrayList)results.values);
+            notifyDataSetChanged();
+        }
+    };
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView stockName;
+        RelativeLayout parentLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            stockName = itemView.findViewById(R.id.stock_name);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+        }
+    }
+}
