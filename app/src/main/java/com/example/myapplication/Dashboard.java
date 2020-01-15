c5468411dc1edde9636644a908e0185645b8ed7d
Faiq <faiqradzali@gmail.com>
2020-01-15 12:23:05 +0800

    Login with firebase authentication, session manager, logout, removed some field from register.xml

--- app/src/main/java/com/example/myapplication/Dashboard.java
+++ app/src/main/java/com/example/myapplication/Dashboard.java
@@ -2,13 +2,42 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.HashMap;
 
 public class Dashboard extends AppCompatActivity {
+
+    private TextView name;
+    private Button btn_logout;
+    SessionManager sessionManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        sessionManager = new SessionManager(this);
+        sessionManager.checkLogin();
+
+        name = findViewById(R.id.text_name);
+        btn_logout = findViewById(R.id.logoutBtn);
+
+        HashMap<String, String> user = sessionManager.getUserDetail();
+        String mName = user.get(sessionManager.NAME);
+        String mEmail = user.get(sessionManager.EMAIL);
+
+        name.setText(mName);
+
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sessionManager.logout();
+            }
+        });
     }
 }
